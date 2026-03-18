92e9f35c6ccf75656dace13f9e809a343c9ec39d
Adithya2310 <adithyanandi099@gmail.com>
2026-03-18 08:55:27 +0530

    update: Implemented proc_write  in the kernel module

--- ldd.c
+++ ldd.c
@@ -25,13 +25,32 @@ static ssize_t custom_read(struct file * file_name, char *user_space_buffer, siz
     return len;
 }
 
+static ssize_t custom_write(struct file * file_name, const char __user *user_space_buffer, size_t count, loff_t* offset){
+    char kernel_buffer[128];
+    size_t len;
+    int res;
+
+    len = count < sizeof(kernel_buffer) - 1 ? count : sizeof(kernel_buffer) - 1;
+
+    if(*offset>=len) 
+        return 0;
+
+    res = copy_from_user(kernel_buffer, user_space_buffer, len);
+    *offset+=len;
+
+    printk(KERN_INFO "KERNEL PROC FS WRITE: %s\n", kernel_buffer);
+    
+    return len;
+}
+
 struct proc_ops driver_proc_ops = {
     .proc_read = custom_read,
+    .proc_write = custom_write
 };
 
 static int kernel_driver_module_init(void){
     printk(KERN_INFO "KERNEL DRIVER LOADED: ENTRY");
-    custom_proc_node = proc_create("kernel_driver", 0, NULL, &driver_proc_ops);
+    custom_proc_node = proc_create("kernel_driver", 0666, NULL, &driver_proc_ops);
 
     if(custom_proc_node == NULL)
     {
