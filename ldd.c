e0b8c1863b23be3d8dc87f1b7bcbe9f7b4f832fd
Adithya2310 <adithyanandi099@gmail.com>
2026-03-18 07:46:55 +0530

    feat: Implement a kernel module with a procfs entry and a Python user-space application for interaction.

---
+++ ldd.c
@@ -0,0 +1,53 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/proc_fs.h>
+
+MODULE_LICENSE("GPL"); //*
+MODULE_AUTHOR("ADITHYA"); //!
+MODULE_DESCRIPTION("A KERNEL DRIVER"); //!
+
+static struct proc_dir_entry *custom_proc_node;
+
+static ssize_t custom_read(struct file * file_name, char *user_space_buffer, size_t count, loff_t* offset){
+    char msg[] = "Ack!\n";
+    size_t len=strlen(msg);
+    int res;
+
+
+    if(*offset>=len) 
+        return 0;
+
+    res = copy_to_user(user_space_buffer, msg, len);
+    *offset+=len;
+
+    printk(KERN_INFO "KERNEL PROC FS READ: ENTRY\n");
+    
+    return len;
+}
+
+struct proc_ops driver_proc_ops = {
+    .proc_read = custom_read,
+};
+
+static int kernel_driver_module_init(void){
+    printk(KERN_INFO "KERNEL DRIVER LOADED: ENTRY");
+    custom_proc_node = proc_create("kernel_driver", 0, NULL, &driver_proc_ops);
+
+    if(custom_proc_node == NULL)
+    {
+        printk(KERN_INFO "KERNEL DRIVER LOADED: EXIT");
+        return -1;
+    }
+
+    printk(KERN_INFO "KERNEL DRIVER LOADED: EXIT");
+    return 0;
+}
+
+static void kernel_driver_module_exit(void){
+    printk(KERN_INFO "KERNEL DRIVER UNLOADED: ENTRY");
+    proc_remove(custom_proc_node);
+    printk(KERN_INFO "KERNEL DRIVER UNLOADED: EXIT");
+}
+
+module_init(kernel_driver_module_init);
+module_exit(kernel_driver_module_exit);
